36ae2ca1014df39f34f0e4a72db1de8bb6582005
Todd Johnson <toddjohnson407@gmail.com>
2020-01-13 15:44:34 -0700

    Configuring app for local testing db.

--- src/config.js
+++ src/config.js
@@ -10,6 +10,22 @@ Firebase.initializeApp({
   storageBucket: "cloned-a5608.appspot.com",
 });
 
+// TESTING LOCAL DB CONFIG
+// function getMoviesFromApiAsync() {
+//   // return fetch('http://127.0.0.1:3000/api/admin/testmobile')
+//   // return fetch('http://localhost:3000/api/admin/testmobile')
+//   return fetch('https://facebook.github.io/react-native/movies.json')
+//     .then((response) => response.json())
+//     .then((responseJson) => {
+//       return responseJson;
+//     })
+//     .catch((error) => {
+//       console.error('ERROR:', error);
+//     });
+// }
+
+// getMoviesFromApiAsync().then(res => console.log('Fetch Request Complete:', res));
+
 export const db = Firebase.firestore();
 export const auth = Firebase.auth();
 export const storage = Firebase.storage();
